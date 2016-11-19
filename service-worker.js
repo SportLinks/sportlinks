f426cadee9817bf36e9a4babd428d4a9d4c8dc05
Jose Manuel Lopez Doña <jmlopezdona@gmail.com>
2016-11-20 00:21:25 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/sportlinks/asset-manifest.json","90ded4b1d5ed073dc1dc29a9eb68876b"],["/sportlinks/index.html","f93b9e76ebe22ce0f155165e16e5492f"],["/sportlinks/static/css/main.65027555.css","41e5e45b9b5d9ecaa09b72c11eed3386"],["/sportlinks/static/css/main.65027555.css.map","897fd36918e2007edab902a79694c131"],["/sportlinks/static/js/main.5a58bc43.js","839d45c02dc6d94a5a51e31c896bdbda"],["/sportlinks/static/js/main.5a58bc43.js.map","ce9ff7f0a2616e997a488b27391059d9"]];
+var precacheConfig = [["/sportlinks/asset-manifest.json","8d5d36b22a9ad0bd4ade4f24325aab3b"],["/sportlinks/index.html","4d9b6aadfcb298efc3e62d733b5e748d"],["/sportlinks/static/css/main.65027555.css","41e5e45b9b5d9ecaa09b72c11eed3386"],["/sportlinks/static/css/main.65027555.css.map","897fd36918e2007edab902a79694c131"],["/sportlinks/static/js/main.de8fff66.js","f8b769e54c2547d2eae94789ac24dc22"],["/sportlinks/static/js/main.de8fff66.js.map","32266c7428da0803bd653b3c7675e0b4"]];
 var cacheName = 'sw-precache-v2-sportlinks-' + (self.registration ? self.registration.scope : '');
 
 
